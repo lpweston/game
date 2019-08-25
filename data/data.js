ccb434c1eb072a3b1bf5004050848ec8f6445a55
jcweston <54505249+jcweston@users.noreply.github.com>
2019-08-25 16:33:54 +0100

    Create Data.js

---
+++ data/data.js
@@ -0,0 +1,12 @@
+Room 1: Escape Pod
+Description: Your escape pod contains crash couches and life support for five people, but you are the only occupant. There is a sealed escape hatch at one end of the pod, and a communications console at the other end. Survival equipment is secured to the walls, and a small porthole is built into the roof.
+Leave Room: Open hatch (without Cutting Torch)
+You try the escape hatch, which should open freely, only to find that it is jammed. On closer inspection, you see that the mechanism has fused from extreme heat, and will need to be cut open
+Choice 1: Send distress signal
+Leave Room: Open hatch (with Cutting Torch)
+You fire up the torch and burn through the fused locking mechanism (using up the torch's limited fuel supply). The hatch swings open with a crash. (-1 Cutting Torch)
+You activate the emergency subspace radio, but hear nothing but static. The equipment must have been damaged in the crash (or is being jammed...)
+Choice 2: Search supplies
+The survival supplies are limited, but you do find a Medical Kit and a Cutting Torch (+1 Medkit, +1 Cutting Torch)
+Choice 3: Look out of porthole
+The porthole in the roof is diffult to access, but by standing on a crash couch you are able to get a look out of it. You see (LANDSCAPE TBD)
