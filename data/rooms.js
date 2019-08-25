c4c86b3dc75dcb418c5085620812a111a939c642
Laura <l.weston@outlook.com>
2019-08-25 17:35:51 +0100

    escape pod intro plays once

--- data/rooms.js
+++ data/rooms.js
@@ -1,17 +1,12 @@
 exports.escapePod = {
   description:
     "Your escape pod contains crash couches and life support for five people, but you are the only occupant. There is a sealed escape hatch at one end of the pod, and a communications console at the other end. Survival equipment is secured to the walls, and a small porthole is built into the roof.",
-  choices: [
-    "Leave Room",
-    "Send Distress Signal",
-    "Search Supplies",
-    "Look Out of Porthole"
-  ],
-  results: {
-    "Leave Room":
+  options: {
+    "Leave Room": [
       "You try the escape hatch, which should open freely, only to find that it is jammed. On closer inspection, you see that the mechanism has fused from extreme heat, and will need to be cut open",
-    "Leave Room B":
-      "You fire up the torch and burn through the fused locking mechanism (using up the torch's limited fuel supply). The hatch swings open with a crash. (-1 Cutting Torch)",
+
+      "You fire up the torch and burn through the fused locking mechanism (using up the torch's limited fuel supply). The hatch swings open with a crash. (-1 Cutting Torch)"
+    ],
     "Send Distress Signal":
       "You activate the emergency subspace radio, but hear nothing but static. The equipment must have been damaged in the crash (or is being jammed...)",
     "Search Supplies":
--- index.js
+++ index.js
@@ -6,21 +6,32 @@ const inventory = {};
 const escapePodChoices = {
   type: "list",
   name: "choice",
-  message: data.rooms.escapePod.description,
-  choices: data.rooms.escapePod.choices
+  message: "What would you like to do?",
+  choices: Object.keys(data.rooms.escapePod.options)
 };
+
+function escapePodEnter() {
+  console.log(data.rooms.escapePod.description);
+  escapePod();
+}
 
 function escapePod() {
   inquirer.prompt(escapePodChoices).then(answer => {
-    if (answer.choice === "Leave Room" && inventory.cuttingTorch) {
-      console.log(data.rooms.escapePod.results["Leave Room B"]);
-      outside();
-    } else if (answer.choice === "Search supplies") {
-      console.log(data.rooms.escapePod.results[answer.choice]);
+    if (answer.choice === "Leave Room") {
+      if (inventory.cuttingTorch) {
+        console.log(data.rooms.escapePod.options["Leave Room"][1]);
+        outside();
+      } else {
+        console.log(data.rooms.escapePod.options["Leave Room"][0]);
+        escapePod();
+      }
+    } else if (answer.choice === "Search Supplies") {
+      console.log(data.rooms.escapePod.options[answer.choice]);
       inventory.cuttingTorch = 1;
+      inventory.medicalKit = 1;
       escapePod();
     } else {
-      console.log(data.rooms.escapePod.results[answer.choice]);
+      console.log(data.rooms.escapePod.options[answer.choice]);
       escapePod();
     }
   });
@@ -30,4 +41,4 @@ function outside() {
   console.log("Well done you win!");
 }
 
-escapePod();
+escapePodEnter();
