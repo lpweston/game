b8c346bd76d79efd26355b7684528584b11c3941
jcweston <54505249+jcweston@users.noreply.github.com>
2019-09-01 18:41:59 +0100

    Added Cargo Bay 1 and Languages

--- db/data/rooms.js
+++ db/data/rooms.js
@@ -36,6 +36,11 @@ exports.escapePodTemplate = {
       description:
         "The porthole in the roof is diffult to access, but by standing on a crash couch you are able to get a look out of it. You see a grey metal roof, criss-crossed with structural supports. Alien symbols appear at regular intervals."
     }
+     },
+    "Look Out of Porthole (Translator)": {
+      description:
+        "The porthole in the roof is diffult to access, but by standing on a crash couch you are able to get a look out of it. You see a grey metal roof, criss-crossed with structural supports. Warning labels appear at regular intervals, indicating that this hanger is designed to hold a maximum of three small craft."
+    }
   },
   doors: {
     "Open Hatch": {
@@ -61,32 +66,29 @@ exports.hanger1Template = {
       description:
         "The shuttlecraft is larger than your escape pod, but not by much. It is sleek and aerodynamic, with bulky sublight engines flaring from the back. You suspect that it is a short range support craft, designed for planetary landings"
     },
-    "Read Alien Text": {
-      description:
-        "The crude and angular characters above the doors are not from a language that you understand. You guess that the text refers to the rooms beyond the two doors, though whether merely a name or some kind of warning, you cannot tell."
-    },
     "Search Lockers": {
       description:
-        "The lockers contain spacesuits, with limbs and bodily proportions strange and surprising to you. Whoever operates this ship does not resemble you in the slightest. One of the spacesuits has a pouch on the front which contains an ID card, which you take.",
-      keys: { idCard1: 1 }
+        "The lockers contain spacesuits, with limbs and bodily proportions strange and surprising to you. Whoever operates this ship does not resemble you in the slightest.",
     }
   },
   doors: {
     "Central Corridor Door": {
-      location: "centralCorridoor",
+      location: "centralCorridor",
       status: "closed",
       key: "repressurisedCC",
       closed:
-        "The hatch is sealed tight. The mechanism appears to be designed to stop the door opening into vacuum.",
+        "The hatch is sealed tight. A screen on the door displays a message, in unreadable alien text.",
+      closedTranslator:
+      "The hatch is sealed tight. A screen on the door displays the message: Hull Breach Detected. Atmosphere Loss Detected. Internal Bulkheads Locked."
       open:
-        "The hatch swings open easily, now that the corridor beyond is pressurised. You move into the corridor beyond.",
+        "The hatch no longer displays the warning message, and swings open easily. You move into the corridor beyond.",
       opened: "You move into the Central Corridor"
     },
     "Cargo Bay 1 Door": {
       location: "cargoBay1",
       status: "closed",
       open:
-        "After a moment's effort, the hatch creaks open and you pass through it.",
+        "The hatch is only partially closed, and the locking mechanisms have not enaged properly. After a moment's effort, the hatch creaks open and you pass through it.",
       opened: "You move into Cargo Bay 1"
     },
     "Shuttle Door": {
@@ -102,7 +104,7 @@ exports.hanger1Template = {
     "Escape Pod": {
       location: "escapePod",
       status: "opened",
-      opened: "You climb in the escape pod"
+      opened: "You climb into the escape pod"
     }
   }
 };
@@ -118,7 +120,7 @@ exports.shuttleTemplate = {
     },
     "Search Lockers": {
       description:
-        "You find a small plastic box labelled with alien script. You open the box, and see that it contains a metal disc. You take the disc out of the box, and while holding it, the text seems suddenly clear and understandable to you. Looking at the box, you see that it is labelled 'Universal Translator'. This will come in very handy, you think.",
+        "You find a small plastic box labelled with alien script. You open the box, and see that it contains a metal disc. You take the disc out of the box. As soon as you touch it, your vision swims and the text on the box blurs, then sharpens again into perfect clarity. To your surprise, you can read the text perfectly. The box is labelled 'Universal Translator'. This will come in very handy, you think.",
       keys: { universalTranslator: 1 }
     }
   },
@@ -127,6 +129,76 @@ exports.shuttleTemplate = {
       location: "hanger1",
       status: "opened",
       opened: "You climb out of the shuttle"
+    }
+  }
+};
+
+exports.cargoBay1Template = {
+  name: "Cargo Bay 1",
+  description: "A long, cavernous room. 5 large metal containers line the outher wall, clearly designed for long-term storage. At the far end of the room, away from the doors, lies a corpse.",
+  options: {
+    "Examine Container 1": {
+      description: "The container is labelled in the alien language, and is locked.",
+    }
+    "Examine Container 1 (Translator)": {
+      description: "The container is labelled 'Classified', and is locked. A keypad is labelled 'level 1 clearance'",
+    }
+      "Examine Container 2": {
+      description: "The container is labelled in the alien language, and is locked.",
+    }
+    "Examine Container 2 (Translator)": {
+      description: "The container is labelled 'Weapons', and is locked. A keypad is labelled 'level 2 clearance '",
+    }
+          "Examine Container 3": {
+      description: "The container is labelled in the alien language. Inside, racks of boxes and large, sealed crates are secured to the walls. Each container is labelled, but you cannot read the language. It would take hours to search this container for useful supplies.",
+    }
+    "Examine Container 3 (Translator)": {
+      description: "The container is labelled 'Emergency Repairs'. Inside, racks of boxes and large, sealed crates are secured to the walls. Each container is labelled according to its contents and the emergency conditions it should be used in. You quickly find a 'Breach Seal Kit'",
+        item: { breachSealKit: 1 }
+    }
+              "Examine Container 4": {
+      description: "The container is labelled in the alien language. Inside, dozens of crates are stacked together, secured with magnetic clamps. The crates contain mechanical and electronic parts, whose function you do not understand. Each container is labelled, but you cannot read the language. You do not think that you will be able to use anything from this container.",
+    }
+    "Examine Container 4 (Translator)": {
+      description: "The container is labelled 'Spare Parts'. Inside, dozens of crates are stacked together, secured with magnetic clamps. The crates contain mechanical and electronic parts, whose function you do not understand. Each container is labelled. You do not think that you will be able to use anything from this container.",
+    }
+            "Examine Container 5": {
+      description: "The container is labelled in the alien language. Inside, dozens of crates are stacked together, secured with magnetic clamps. The first crate you open contains tubes of strange liquids, boxes of soft, brightly coloured orbs, and red, fleshy chunks wrapped in clear plastic. Each item is labelled, but you cannot read the language.",
+    }
+    "Examine Container 5 (Translator)": {
+      description: "The container is labelled 'Food'. Inside, dozens of crates are stacked together, secured with magnetic clamps. The first crate you open contains tubes of strange liquids, boxes of soft, brightly coloured orbs, and red, fleshy chunks wrapped in clear plastic. Each item is labelled, indicating that these substances provide nutrition for the crew of this spacecraft. Looking closer, you see that many of these foods would be toxic to you.",
+    }
+"Examine Corpse 1: {
+      description: "A body lies slumped against the wall. You are not exactly sure what this creature looked like in life, but one of its many appendages appears to end in a bloody mess. As you draw near, you begin to hear a whistling sound, which grows to a low roar the closer you get. Looking around, you soon identify the source: a small breach in the hull, near the body. There is a matching hole in the opposite wall, leading to the interior of the ship. You suspect that whatever projectile did this damage, also killed the alien. Tentatively searching the corpse, you find some kind of key card held in their hand.",
+      keys: { idCard1: "+1" }
+    }
+              "Examine Corpse 2: {
+      description: "The alien body lies slumped and unmoving.",
+    }
+      "Seal Breach (Needs breachSealKit): {
+      description: "You clamp the thin metal disk of the seal kit to the breach, and hit the activation button. Sparks fly as the patch welds itself to the surrounding wall, and the sound of escaping air abruptly fades. You hear a faint clunk, as the locking mechanism on the doors retract, in response to the rising air pressure.",
+      keys: { idCard1: "+1" }
+    }
+  },
+  doors: {
+    "Hanger 1": {
+      status: "open",
+      closed: "Text",
+      open: "You move into Hanger 1",
+      opened: "You move into Hanger 1"
+    }
+    {
+    "Central Corridor Door": {
+      location: "centralCorridor",
+      status: "closed",
+      key: "repressurisedCC",
+      closed:
+        "The hatch is sealed tight. A screen on the door displays a message, in unreadable alien text.",
+      closedTranslator:
+      "The hatch is sealed tight. A screen on the door displays the message: Hull Breach Detected. Atmosphere Loss Detected. Internal Bulkheads Locked."
+      open:
+        "The hatch no longer displays the warning message, and swings open easily. You move into the corridor beyond.",
+      opened: "You move into the Central Corridor"
     }
   }
 };
